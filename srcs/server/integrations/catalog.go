e9fe2d3efd3d6f8215a73c6d7de5e2096943c539
Kevin Wang <kevixw@gmail.com>
2026-03-30 23:40:11 -0700

    feat: implement orchestration service and agent integration framework (#712)

---
+++ srcs/server/integrations/catalog.go
@@ -0,0 +1,34 @@
+package integrations
+
+import (
+	pb "github.com/onehumancorp/mono/srcs/proto"
+)
+
+// Integration represents a plugin blueprint defining how to instantiate connections.
+type Integration interface {
+	Metadata() *pb.IntegrationMetadata
+	WizardSteps() []*pb.WizardStep
+}
+
+// Catalog holds the global static list of all supported integration providers.
+var Catalog = []Integration{
+	&SlackIntegration{},
+	&DiscordIntegration{},
+	&GoogleChatIntegration{},
+	&TelegramIntegration{},
+	&TeamsIntegration{},
+	&WhatsAppIntegration{},
+	&IMessageIntegration{},
+	&GitHubIntegration{},
+	&GitLabIntegration{},
+	&GiteaIntegration{},
+	&JiraIntegration{},
+	&PlaneIntegration{},
+	&GitHubIssuesIntegration{},
+	&LinearIntegration{},
+}
+
+// GetCatalog returns all available integration providers.
+func GetCatalog() []Integration {
+	return Catalog
+}
