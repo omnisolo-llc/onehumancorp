4d3fecbe1b0703c6007292371fc76b4f7faf5b6e
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-10 07:30:14 +0000

    Fix KAIROS Shared Tasks schema and compilation errors

--- fix_test_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/TaskOrchestrator/SharedTaskDBManager/g' srcs/server/orchestration/tasks_db_test.go
-sed -i 's/TaskOrchestrator/SharedTaskDBManager/g' srcs/server/orchestration/tasks_db_ext.go
