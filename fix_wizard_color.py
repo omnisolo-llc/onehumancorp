8ce920d3555d5e41d8c3e9002d85b0417833576a
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-10 06:39:49 +0000

    Apply Glassmorphism to remaining Cards across screens

--- fix_wizard_color.py
+++
@@ -1,8 +0,0 @@
-import re
-
-filepath = "srcs/app/lib/screens/wizard_screen.dart"
-
-with open(filepath, 'r') as f:
-    content = f.read()
-
-# We need to restore the wizard_screen.dart from git and re-apply correctly
