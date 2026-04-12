8748fa1b74f6391bc7e70c87a597a56e76a26467
Kevin Wang <kevixw@gmail.com>
2026-04-11 21:15:09 -0700

    refactor: reorder import statements and add Ping method to mockProvider

--- srcs/server/orchestration/health_test.go
+++ srcs/server/orchestration/health_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 	"time"
 
-	_ "modernc.org/sqlite"
 	"github.com/onehumancorp/mono/srcs/server/db"
+	_ "modernc.org/sqlite"
 )
 
 func TestHybridHealthProbe(t *testing.T) {
@@ -145,7 +145,6 @@ func (m *mockProvider) Exec(ctx context.Context, sql string, arguments ...any) (
 	return 1, nil
 }
 
-
 func (m *mockProvider) Ping(ctx context.Context) error {
 	if m.pingErr != nil {
 		return m.pingErr
@@ -164,8 +163,7 @@ func (m *mockProvider) QueryRow(ctx context.Context, sql string, optionsAndArgs 
 	return &mockRow{}
 }
 
-type mockRow struct {
-}
+type mockRow struct{}
 
 func (r *mockRow) Scan(dest ...any) error {
 	*dest[0].(*int) = 5
