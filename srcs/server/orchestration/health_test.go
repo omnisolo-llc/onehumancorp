20a11a997bbede29f531095f3bc510374fef43bf
Kevin Wang <kevixw@gmail.com>
2026-04-12 10:30:19 -0700

    Revert "🧹 Maintainer: Fix test failure in orchestration_test" (#3556)
    Reverts onehumancorp/mono#3388

--- srcs/server/orchestration/health_test.go
+++ srcs/server/orchestration/health_test.go
@@ -141,10 +141,6 @@ func (m *mockProvider) Ping(ctx context.Context) error {
 	return m.execErr
 }
 
-func (m *mockProvider) Ping(ctx context.Context) error {
-	return m.execErr
-}
-
 func (m *mockProvider) Exec(ctx context.Context, sql string, arguments ...any) (int64, error) {
 	if m.execErr != nil {
 		return 0, m.execErr
