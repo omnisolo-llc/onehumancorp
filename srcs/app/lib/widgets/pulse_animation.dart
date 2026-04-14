14874bec31010954cb98a50f2e150b2134bec626
Kevin Wang <kevixw@gmail.com>
2026-04-14 03:35:44 -0700

    🎨 Palette: Add PulseAnimation micro-animation (#4484)
    Adds a high-fidelity PulseAnimation widget to satisfy the
    micro-animation requirements for OHC agent message states. Also properly
    relocates the incorrectly placed prompt_tuning_wizard_screen_test.dart
    from lib/screens to test/screens.
    
    ---
    *PR created automatically by Jules for task
    started by @ql-owo-lp*

---
+++ srcs/app/lib/widgets/pulse_animation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class PulseAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+
+  const PulseAnimation({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 1000),
+  });
+
+  @override
+  State<PulseAnimation> createState() => _PulseAnimationState();
+}
+
+class _PulseAnimationState extends State<PulseAnimation> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.duration)..repeat(reverse: true);
+    _animation = Tween<double>(begin: 1.0, end: 1.05).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _animation,
+      child: widget.child,
+    );
+  }
+}
