3449b248e0cae11425f1f9257487aed2f2c9cebf
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-15 06:07:20 +0000

    ✨ Implementer: Missing Standalone BufferMetricFunc in Telemetry Extensions

--- srcs/server/telemetry/telemetry_bridge.go
+++ srcs/server/telemetry/telemetry_bridge.go
@@ -2,6 +2,7 @@ package telemetry
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -50,17 +51,32 @@ func RecordBridgeMessageSent(ctx context.Context) {
 	if bridgeMessagesSentTotal != nil {
 		bridgeMessagesSentTotal.Add(ctx, 1)
 	}
+	if BufferMetricFunc != nil {
+		payloadMap := map[string]interface{}{}
+		payloadBytes, _ := json.Marshal(RedactInterfacePII(payloadMap))
+		_ = BufferMetricFunc(ctx, "bridge_message_sent", string(payloadBytes))
+	}
 }
 
 func RecordBridgeMessageReceived(ctx context.Context) {
 	if bridgeMessagesReceivedTotal != nil {
 		bridgeMessagesReceivedTotal.Add(ctx, 1)
 	}
+	if BufferMetricFunc != nil {
+		payloadMap := map[string]interface{}{}
+		payloadBytes, _ := json.Marshal(RedactInterfacePII(payloadMap))
+		_ = BufferMetricFunc(ctx, "bridge_message_received", string(payloadBytes))
+	}
 }
 
 func RecordBridgeStatus(ctx context.Context, active int64) {
 	if bridgeStatusGauge != nil {
 		bridgeStatusGauge.Add(ctx, active)
+	}
+	if BufferMetricFunc != nil {
+		payloadMap := map[string]interface{}{"active_count": active}
+		payloadBytes, _ := json.Marshal(RedactInterfacePII(payloadMap))
+		_ = BufferMetricFunc(ctx, "bridge_status", string(payloadBytes))
 	}
 }
 
