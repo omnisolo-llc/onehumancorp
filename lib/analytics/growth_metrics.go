ea3904dc6023011ac6b122837c0b6ae0564d1ef8
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-16 01:44:21 +0000

    🚀 Nova: Implement Free-Tier Quota System

--- lib/analytics/growth_metrics.go
+++ lib/analytics/growth_metrics.go
@@ -9,8 +9,6 @@ import (
 var (
 	inviteSentCounter     metric.Int64Counter
 	inviteAcceptedCounter metric.Int64Counter
-	quotaExceededCounter metric.Int64Counter
-	quotaPassedCounter metric.Int64Counter
 )
 
 func init() {
@@ -21,14 +19,6 @@ func init() {
 		panic(err)
 	}
 	inviteAcceptedCounter, err = meter.Int64Counter("growth_viral_invite_accepted_total")
-	if err != nil {
-		panic(err)
-	}
-	quotaExceededCounter, err = meter.Int64Counter("growth_quota_exceeded_total")
-	if err != nil {
-		panic(err)
-	}
-	quotaPassedCounter, err = meter.Int64Counter("growth_quota_passed_total")
 	if err != nil {
 		panic(err)
 	}
@@ -46,9 +36,5 @@ func (t *Tracker) TrackEvent(ctx context.Context, name string, props map[string]
 		inviteSentCounter.Add(ctx, 1)
 	} else if name == "invite_accepted" && inviteAcceptedCounter != nil {
 		inviteAcceptedCounter.Add(ctx, 1)
-	} else if name == "quota_exceeded" && quotaExceededCounter != nil {
-		quotaExceededCounter.Add(ctx, 1)
-	} else if name == "quota_check_passed" && quotaPassedCounter != nil {
-		quotaPassedCounter.Add(ctx, 1)
 	}
 }
