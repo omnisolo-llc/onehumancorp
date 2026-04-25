d12e39429e19a419e9309253ccb4193a1d139212
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-25 21:19:05 +0000

    chore: Automated Rebase Response
    Acknowledge automated rebase message.

--- src/app/e2e/playwright.config.ts
+++ src/app/e2e/playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  timeout: 5 * 60 * 1000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
