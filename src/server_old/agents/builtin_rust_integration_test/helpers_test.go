4db6e929ad679fb825b499d83fcb3cbebf8a8c81
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-26 23:38:13 +0000

    Fix terminal test bazel target and rust binary test paths

--- src/server_old/agents/builtin_rust_integration_test/helpers_test.go
+++ src/server_old/agents/builtin_rust_integration_test/helpers_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	binaryRunpath = "_main/src/server/src/agents/builtin/ohc-builtin-agent"
+	binaryRunpath = "mono/src/server/src/agents/builtin/ohc-builtin-agent"
 	startTimeout  = 10 * time.Second
 	rpcTimeout    = 30 * time.Second
 )
