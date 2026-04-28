1131eef39fd7d711191af6650f1d28f3a80708ee
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-28 02:34:47 +0000

    💰 Miser: Add cost dashboard and plan UI features

--- src/server/build.rs
+++ src/server/build.rs
@@ -4,9 +4,7 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set PROTOC environment variable to point to the built protoc in runfiles
     let protoc_path = PathBuf::from("../../../protobuf+/protoc");
-    unsafe {
-        env::set_var("PROTOC", protoc_path);
-    }
+    env::set_var("PROTOC", protoc_path);
     
     println!("cargo:warning=Set PROTOC to {:?}", env::var("PROTOC"));
 
