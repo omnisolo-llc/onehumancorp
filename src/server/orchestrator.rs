8e55b1d07f6a4ef9743bada2ea4879eaa3a23f3a
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-04-28 12:04:10 +0000

    Implement KAIROS Orchestration Backend APIs

--- src/server/orchestrator.rs
+++ src/server/orchestrator.rs
@@ -152,6 +152,7 @@ pub fn start_token_burn_forecaster(
 
         loop {
             interval.tick().await;
+            println!("TokenBurnForecaster: tick");
 
             let orgs = vec!["org1".to_string(), "org2".to_string()];
             
