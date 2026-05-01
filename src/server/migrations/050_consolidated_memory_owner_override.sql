cc4e7ef53d434dbd6782bc816cbcff21d25d21a7
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-05-01 14:55:57 +0000

    Cleanup temp files

--- src/server/migrations/050_consolidated_memory_owner_override.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-ALTER TABLE consolidated_memory ADD COLUMN IF NOT EXISTS owner_override BOOLEAN DEFAULT FALSE;
-
--- +goose Down
-ALTER TABLE consolidated_memory DROP COLUMN IF EXISTS owner_override;
