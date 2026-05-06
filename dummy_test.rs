4f223eb6fc9b6156511f2854a8418bbd7268fdc3
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-05-06 09:00:46 +0000

    feat: Ensure test parity for Consolidator Memory Layer

--- dummy_test.rs
+++
@@ -1,9 +0,0 @@
-#[tokio::main]
-async fn main() {
-    use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-    use std::str::FromStr;
-
-    let conn_opts = SqliteConnectOptions::from_str("sqlite::memory:").unwrap();
-    let res = SqlitePoolOptions::new().connect_with(conn_opts).await;
-    println!("{:?}", res);
-}
