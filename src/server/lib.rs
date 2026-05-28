1456912eb1ac5ab28b236ebe6fd99baa3c9e1ca6
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-05-28 12:51:28 +0000

    Add 100th order success milestone with viral share loop

--- src/server/lib.rs
+++ src/server/lib.rs
@@ -460,7 +460,7 @@ async fn http_login_handler(
         }
     };
 
-    let _claims = ::server_common::Claims {
+    let claims = ::server_common::Claims {
         sub: id.clone(),
         exp: expires_at,
         iat: issued_at,
