240309c5632da18380175310244bf80e02426d4f
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-05-28 19:07:12 +0000

    fix(bazel): skip non-linux tests, suppress ambiguous glob exports, fix tauri icons

--- src/server/lib.rs
+++ src/server/lib.rs
@@ -1,3 +1,5 @@
+#![allow(ambiguous_glob_reexports)]
+#![allow(ambiguous_glob_imports)]
 pub use ::server_harness as harness;
 pub mod api;
 
--- src/server/tools/bazel_lib.rs
+++ src/server/tools/bazel_lib.rs
@@ -1,3 +1,5 @@
+#![allow(ambiguous_glob_reexports)]
+#![allow(ambiguous_glob_imports)]
 #![allow(unused_imports)]
 
 pub use ::server_lib::*;
