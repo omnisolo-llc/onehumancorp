79bf15ac4ba8604c1ee3ebbf86d32c46db418ce8
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-06-02 13:24:16 +0000

    fix(e2e): correctly utilize memberPage test fixture

--- src/e2e/test_services_billing.spec.ts
+++ src/e2e/test_services_billing.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from './fixtures';
 import { memberPage } from './fixtures';
 
 test.describe('Pricing & Billing Portal', () => {
