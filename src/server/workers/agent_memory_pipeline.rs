f89ed4ec3ca5210b64e0ff0b3ffa189488964655
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-06-11 02:00:02 +0000

    Fix emb_str compiler errors and unused StatusCode import

--- src/server/workers/agent_memory_pipeline.rs
+++ src/server/workers/agent_memory_pipeline.rs
@@ -60,7 +60,7 @@ impl AgentMemoryPipeline {
                         }
                     };
 
-                    let emb_str = format!("[{}]", embedding.iter().map(|f| f.to_string()).collect::<Vec<_>>().join(","));
+                    let _emb_str = format!("[{}]", embedding.iter().map(|f| f.to_string()).collect::<Vec<_>>().join(","));
                     let mem_id = Uuid::new_v4();
 
                     sqlx::query("INSERT INTO consolidated_memory (id, tenant_id, agent_id, source_type, content, embedding) VALUES ($1, $2, $3, $4, $5, NULL)")
@@ -103,7 +103,7 @@ impl AgentMemoryPipeline {
                         }
                     };
 
-                    let emb_str = format!("[{}]", embedding.iter().map(|f| f.to_string()).collect::<Vec<_>>().join(","));
+                    let _emb_str = format!("[{}]", embedding.iter().map(|f| f.to_string()).collect::<Vec<_>>().join(","));
                     let mem_id = Uuid::new_v4();
 
                     let mut tx = self.db.pool.begin().await?;
@@ -115,7 +115,7 @@ impl AgentMemoryPipeline {
                         .bind(&agent_id)
                         .bind("SESSION_DATA")
                         .bind(&context_data)
-                        .bind(&emb_str)
+
                         .execute(&mut *tx)
                         .await?;
 
@@ -149,7 +149,7 @@ impl AgentMemoryPipeline {
 
                 match self.embedding_api.generate_embedding(&content).await {
                     Ok(embedding) => {
-                        let emb_str = format!("[{}]", embedding.iter().map(|f| f.to_string()).collect::<Vec<_>>().join(","));
+                        let _emb_str = format!("[{}]", embedding.iter().map(|f| f.to_string()).collect::<Vec<_>>().join(","));
                         let mem_id = Uuid::new_v4();
 
                         match &self.db.store {
@@ -170,7 +170,7 @@ impl AgentMemoryPipeline {
                                     .bind("fs-agent")
                                     .bind("FS_MEMORY")
                                     .bind(&content)
-                                    .bind(&emb_str)
+
                                     .execute(&self.db.pool)
                                     .await?;
                             }
