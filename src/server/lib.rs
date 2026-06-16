ea4bb8e0bf6fd995d62dbabfc274f5b951532a82
ql-owo-lp <kevixw@gmail.com>
2026-06-16 00:10:38 -0700

    Fix Tooltip Registry Route (#28912)
    Fixes missing `tooltip-registry.html` routing that caused e2e test
    failures.
    
    ---
    *PR created automatically by Jules for task
    [7633611329310439742](https://jules.google.com/task/7633611329310439742)
    started by @ql-owo-lp*

--- src/server/lib.rs
+++ src/server/lib.rs
@@ -6108,6 +6108,9 @@ async fn create_ui_bom_item_handler(
         .route("/api/ui/api-docs.html", axum::routing::get(|| async {
             axum::response::Html(include_str!("../ui/tauri/src/ui/api-docs.html"))
         }))
+        .route("/api/ui/tooltip-registry.html", axum::routing::get(|| async {
+            axum::response::Html(include_str!("../ui/tauri/src/ui/tooltip-registry.html"))
+        }))
         .route("/api/ui/changelog.html", axum::routing::get(|| async {
             axum::response::Html(include_str!("../ui/next/public/api/ui/changelog.html"))
         }))
