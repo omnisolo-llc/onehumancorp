4565335b238cdc0929b5072a4f807f8052d91f5f
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-07-13 11:24:24 +0000

    refactor(sre): categorize error logs in hybrid sync daemon

--- src/server/orchestration/hybrid_sync/daemon.rs
+++ src/server/orchestration/hybrid_sync/daemon.rs
@@ -22,7 +22,7 @@ impl HybridSyncDaemon {
     pub async fn run(&self) {
         loop {
             if let Err(e) = self.sync_step().await {
-                error!("[bug] Hybrid sync daemon error: {}", e);
+                error!("Hybrid sync daemon error: {}", e);
                 let _ = ::server_telemetry::record_sync_daemon_error_total(
                     &self.pg_pool,
                     1.0,
@@ -32,7 +32,7 @@ impl HybridSyncDaemon {
                 .await;
             }
             if let Err(e) = self.sync_cloud_escalations().await {
-                error!("[bug] Hybrid sync cloud escalations error: {}", e);
+                error!("Hybrid sync cloud escalations error: {}", e);
                 let _ = ::server_telemetry::record_sync_daemon_error_total(
                     &self.pg_pool,
                     1.0,
@@ -42,7 +42,7 @@ impl HybridSyncDaemon {
                 .await;
             }
             if let Err(e) = self.sync_telemetry_step().await {
-                error!("[bug] Hybrid sync telemetry error: {}", e);
+                error!("Hybrid sync telemetry error: {}", e);
                 let _ = ::server_telemetry::record_sync_daemon_error_total(
                     &self.pg_pool,
                     1.0,
@@ -53,7 +53,7 @@ impl HybridSyncDaemon {
             }
 
             if let Err(e) = self.sync_pos_offline_transactions().await {
-                error!("[bug] Hybrid sync pos offline transactions error: {}", e);
+                error!("Hybrid sync pos offline transactions error: {}", e);
                 let _ = ::server_telemetry::record_sync_daemon_error_total(
                     &self.pg_pool,
                     1.0,
