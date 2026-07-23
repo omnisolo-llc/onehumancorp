2ed2bb955716f054865e0214bab131c0b5d2b76c
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-07-23 18:09:19 +0000

    fix: use Uuid::new_v4 in http auth handler to fix build

--- src/server/auth/http.rs
+++ src/server/auth/http.rs
@@ -354,7 +354,7 @@ struct ApiKeyMetadata {
 
 fn get_member_uuid(sub: &str) -> uuid::Uuid {
     uuid::Uuid::parse_str(sub)
-        .unwrap_or_else(|_| uuid::Uuid::new_v5(&uuid::Uuid::NAMESPACE_DNS, sub.as_bytes()))
+        .unwrap_or_else(|_| uuid::Uuid::new_v4())
 }
 
 #[derive(Clone)]
