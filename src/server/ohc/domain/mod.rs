52db804fdf68ab84bcd382686d64efcf30d55d0d
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-07-26 09:07:27 +0000

    feat: implement native Rust omnichannel chat domain models

--- src/server/ohc/domain/mod.rs
+++ src/server/ohc/domain/mod.rs
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
+=======
 pub mod contact;
 pub mod conversation;
 pub mod inbox;
 pub mod message;
+>>>>>>> df561728d (feat: implement native Rust omnichannel chat domain models)
 pub mod staff_tasks;
